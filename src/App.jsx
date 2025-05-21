d14b82cb19ae13ca5effeaddba4ee016c402097c
yash-sharma125 <sharmayash4505@gmail.com>
2025-05-21 15:29:34 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,13 @@ function App() {
   const [value, setvalue] = useState(0);
 
   return (
+    <>
   <h1>Count{value}</h1>
   <button onClick={
-    {}=>{}
-   setvalue(value+1)
+    ()=>{   setvalue(value+1)
+    }
   }>Add</button>
+</>
   )
 }
-
 export default App
